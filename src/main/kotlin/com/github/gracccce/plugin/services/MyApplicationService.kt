4e6541b6a77b0bd783280962e6a43ae06f1641ca
GitHub Action <action@github.com>
2020-12-02 11:27:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/gracccce/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gracccce/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gracccce.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gracccce.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/gracccce/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gracccce/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gracccce.plugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gracccce.plugin.MyBundle
 
 class MyProjectService(project: Project) {
 
